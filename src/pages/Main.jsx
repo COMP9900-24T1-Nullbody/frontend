1e107665ca8ab70cf34c86e1f8a1e6ee833c8ea9
Shi Tong Yuan <z5192519@ad.unsw.edu.au>
2024-02-24 16:27:51 +1100

    添加了一些图表的基础demo

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -4,11 +4,26 @@ import NavBar from "../components/NavBar";
 import CollapseSiderMenu from "../components/CollapseSideMenu";
 import { Box } from "@mui/material";
 
+import ViewTable from "../components/ViewTable";
+import ViewBarChart from "../components/ViewBarChart";
+import ViewLineChart from "../components/ViewLineChart";
+import ViewPieChart from "../components/ViewPieChart";
+
 export default function Main() {
   return (
     <Box>
       <NavBar />
-      <CollapseSiderMenu />
+      <Box sx={{ display: "flex" }}>
+        <Box>
+          <CollapseSiderMenu />
+        </Box>
+        <Box flexGrow={1}>
+          <ViewTable />
+          <ViewBarChart />
+          <ViewLineChart />
+          <ViewPieChart />
+        </Box>
+      </Box>
     </Box>
   );
 }
