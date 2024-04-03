091b9ca40ad292451a367f2122f9a1430ae50c4e
Shi Tong Yuan <z5192519@ad.unsw.edu.au>
2024-04-03 11:15:37 +1100

    /user/profile页面的用户信息可以正常编辑和更新到后端了

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { jwtDecode } from "jwt-decode";
@@ -24,12 +25,16 @@ import UploadIcon from "@mui/icons-material/Upload";
 import { Theme } from "../theme/main";
 import Image01 from "../img/1.jpg";
 
+import ErrorIcon from "@mui/icons-material/Cancel";
+import CorrectIcon from "@mui/icons-material/CheckCircle";
+
 import config from "../config.json";
 import { LoginSocialGoogle, LoginSocialMicrosoft } from "reactjs-social-login";
 import {
   GoogleLoginButton,
   MicrosoftLoginButton
 } from "react-social-login-buttons";
+import { green } from "@mui/material/colors";
 
 export default function UserProfile() {
   const [themeColor, setThemeColor] = useState("");
@@ -224,13 +229,29 @@ function ProfileAvatar({ imageSrc, setImageSrc }) {
 }
 
 function ProfileForm({ userInfo, setUserInfo }) {
-  const [editName, setEditName] = React.useState(false);
-  const [editEmail, setEditEmail] = React.useState(false);
-  const [editPassword, setEditPassword] = React.useState(false);
+  const [nameEditable, setNameEditable] = React.useState(false);
+  const [emailEditable, setEmailEditable] = React.useState(false);
+  const [passwordEditable, setPasswordEditable] = React.useState(false);
 
-  const handleEditName = () => setEditName((show) => !show);
-  const handleEditEmail = () => setEditEmail((show) => !show);
-  const handleEditPassword = () => setEditPassword((show) => !show);
+  // error/helperText config
+  const [EmailError, setEmailError] = useState(false);
+  const [EmailMessage, setEmailMessage] = useState("");
+
+  const [PasswordLenError, setPasswordLenError] = useState(false);
+  const PasswordLenMessage = "Length is 8-16 characters.";
+
+  const [PasswordLowError, setPasswordLowError] = useState(false);
+  const PasswordLowMessage = "At least one lowercase letter.";
+
+  const [PasswordUpperError, setPasswordUpperError] = useState(false);
+  const PasswordUpperMessage = "At least one uppercase letter.";
+
+  const [PasswordSpecialError, setPasswordSpecialError] = useState(false);
+  const PasswordSpecialMessage = "At least one special character.";
+
+  const handleEditName = () => setNameEditable((show) => !show);
+  const handleEditEmail = () => setEmailEditable((show) => !show);
+  const handleEditPassword = () => setPasswordEditable((show) => !show);
 
   const handleMouseDown = (event) => {
     event.preventDefault();
@@ -253,28 +274,234 @@ function ProfileForm({ userInfo, setUserInfo }) {
   };
 
   // 更改谷歌账户link
-  const handleGoogleLinkChange = (email) => {
+  const handleGoogleLinkChange = (google_id) => {
     const updatedUserInfo = {
       ...userInfo,
       Linked_Account: {
         ...userInfo.Linked_Account,
-        Google: email
+        Google: google_id
       }
     };
     setUserInfo(updatedUserInfo);
   };
 
   // 更改微软账户link
-  const handleMicrosoftLinkChange = (email) => {
+  const handleMicrosoftLinkChange = (microsoft_id) => {
     const updatedUserInfo = {
       ...userInfo,
       Linked_Account: {
         ...userInfo.Linked_Account,
-        Microsoft: email
+        Microsoft: microsoft_id
       }
     };
     setUserInfo(updatedUserInfo);
   };
+
+  const updateName = () => {
+    const request = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        name: userInfo.Name,
+        token: localStorage.getItem("token")
+      })
+    };
+
+    fetch(`${config.BACKEND_URL}/update/name`, request)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.error) {
+          alert(data.error);
+        } else {
+          alert(data.message);
+          localStorage.setItem("token", data.token);
+        }
+      })
+      .catch((error) => {
+        alert("Error: Update name failed, Please check server status.");
+        console.error("Error:", error);
+      });
+  };
+
+  const updateEmail = () => {
+    if (validateEmail(userInfo.Email)) {
+      const request = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          email: userInfo.Email,
+          token: localStorage.getItem("token")
+        })
+      };
+
+      fetch(`${config.BACKEND_URL}/update/email`, request)
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.error) {
+            alert(data.error);
+          } else {
+            alert(data.message);
+            localStorage.setItem("token", data.token);
+          }
+        })
+        .catch((error) => {
+          alert("Error: Update email failed, Please check server status.");
+          console.error("Error:", error);
+        });
+
+      return true;
+    } else {
+      alert("Please check email helperText, and fix it.");
+
+      return false;
+    }
+  };
+
+  const updatePassword = () => {
+    const checkPasswordLen = validatePasswordLen(userInfo.Password);
+    const checkPasswordLow = validatePasswordLow(userInfo.Password);
+    const checkPasswordUpper = validatePasswordUpper(userInfo.Password);
+    const checkPasswordSpecial = validatePasswordSpecial(userInfo.Password);
+
+    if (
+      checkPasswordLen &&
+      checkPasswordLow &&
+      checkPasswordUpper &&
+      checkPasswordSpecial
+    ) {
+      const request = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          password: userInfo.Password,
+          token: localStorage.getItem("token")
+        })
+      };
+
+      fetch(`${config.BACKEND_URL}/update/password`, request)
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.error) {
+            alert(data.error);
+          } else {
+            alert(data.message);
+            localStorage.setItem("token", data.token);
+          }
+        })
+        .catch((error) => {
+          alert("Error: Update password failed, Please check server status.");
+          console.error("Error:", error);
+        });
+
+      return true;
+    } else {
+      alert("Please check password helperText, and fix it.");
+
+      return false;
+    }
+  };
+
+  const updateGoogleLink = (google_id) => {
+    const request = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        google_id: google_id,
+        token: localStorage.getItem("token")
+      })
+    };
+
+    fetch(`${config.BACKEND_URL}/update/google_id`, request)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.error) {
+          alert(data.error);
+        } else {
+          alert(data.message);
+          handleGoogleLinkChange(google_id);
+          localStorage.setItem("token", data.token);
+        }
+      })
+      .catch((error) => {
+        alert("Error: Update Google link failed, Please check server status.");
+        console.error("Error:", error);
+      });
+  };
+
+  const updateMicrosoftLink = (microsoft_id) => {
+    const request = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        microsoft_id: microsoft_id,
+        token: localStorage.getItem("token")
+      })
+    };
+
+    fetch(`${config.BACKEND_URL}/update/microsoft_id`, request)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.error) {
+          alert(data.error);
+        } else {
+          alert(data.message);
+          handleMicrosoftLinkChange(microsoft_id);
+          localStorage.setItem("token", data.token);
+        }
+      })
+      .catch((error) => {
+        alert(
+          "Error: Update Microsoft link failed, Please check server status."
+        );
+        console.error("Error:", error);
+      });
+  };
+
+  useEffect(() => {
+    if (validateEmail(userInfo.Email)) {
+      setEmailError(false);
+      setEmailMessage("Valid Email format!");
+    } else {
+      setEmailError(true);
+      setEmailMessage("Invalid Email format!");
+    }
+  }, [userInfo.Email]);
+
+  useEffect(() => {
+    if (validatePasswordLen(userInfo.Password)) {
+      setPasswordLenError(false);
+    } else {
+      setPasswordLenError(true);
+    }
+
+    if (validatePasswordLow(userInfo.Password)) {
+      setPasswordLowError(false);
+    } else {
+      setPasswordLowError(true);
+    }
+
+    if (validatePasswordUpper(userInfo.Password)) {
+      setPasswordUpperError(false);
+    } else {
+      setPasswordUpperError(true);
+    }
+
+    if (validatePasswordSpecial(userInfo.Password)) {
+      setPasswordSpecialError(false);
+    } else {
+      setPasswordSpecialError(true);
+    }
+  }, [userInfo.Password]);
 
   return (
     <Stack spacing={2} sx={{ marginTop: 2, width: "70%" }}>
@@ -284,17 +511,22 @@ function ProfileForm({ userInfo, setUserInfo }) {
         <OutlinedInput
           id="name"
           label="Name"
-          disabled={editName ? false : true}
+          disabled={nameEditable ? false : true}
           value={userInfo.Name}
           onChange={handleNameChange}
           endAdornment={
             <InputAdornment position="end">
               <IconButton
-                onClick={handleEditName}
+                onClick={() => {
+                  if (nameEditable) {
+                    updateName();
+                  }
+                  handleEditName();
+                }}
                 onMouseDown={handleMouseDown}
                 edge="end"
               >
-                {editName ? <Save /> : <Edit />}
+                {nameEditable ? <Save /> : <Edit />}
               </IconButton>
             </InputAdornment>
           }
@@ -307,21 +539,48 @@ function ProfileForm({ userInfo, setUserInfo }) {
         <OutlinedInput
           id="email"
           label="Email"
-          disabled={editEmail ? false : true}
+          disabled={emailEditable ? false : true}
           value={userInfo.Email}
           onChange={handleEmailChange}
           endAdornment={
             <InputAdornment position="end">
               <IconButton
-                onClick={handleEditEmail}
+                onClick={() => {
+                  let updateResult = true;
+                  if (emailEditable) {
+                    updateResult = updateEmail();
+                  }
+                  if (updateResult) {
+                    handleEditEmail();
+                  }
+                }}
                 onMouseDown={handleMouseDown}
                 edge="end"
               >
-                {editEmail ? <Save /> : <Edit />}
+                {emailEditable ? <Save /> : <Edit />}
               </IconButton>
             </InputAdornment>
           }
         />
+        <FormHelperText error={EmailError}>
+          {EmailError ? (
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+              <ErrorIcon fontSize="small" /> {EmailMessage}
+            </Box>
+          ) : userInfo.Email !== "" ? (
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                color: green[500]
+              }}
+            >
+              <CorrectIcon fontSize="small" /> {EmailMessage}
+            </Box>
+          ) : (
+            ""
+          )}
+        </FormHelperText>
       </FormControl>
 
       {/* Password */}
@@ -330,26 +589,110 @@ function ProfileForm({ userInfo, setUserInfo }) {
         <OutlinedInput
           id="password"
           label="Password"
-          type={editPassword ? "text" : "password"}
-          disabled={editPassword ? false : true}
+          type={passwordEditable ? "text" : "password"}
+          disabled={passwordEditable ? false : true}
           value={userInfo.Password}
           onChange={handlePasswordChange}
           endAdornment={
             <InputAdornment position="end">
               <IconButton
-                onClick={handleEditPassword}
+                onClick={() => {
+                  let updateResult = true;
+                  if (passwordEditable) {
+                    updateResult = updatePassword();
+                  }
+                  if (updateResult) {
+                    handleEditPassword();
+                  }
+                }}
                 onMouseDown={handleMouseDown}
                 edge="end"
               >
-                {editPassword ? <Save /> : <Edit />}
+                {passwordEditable ? <Save /> : <Edit />}
               </IconButton>
             </InputAdornment>
           }
         />
-        {userInfo.Password === "" && (
+        {userInfo.Password === "" ? (
           <FormHelperText id="component-error-text">
             Empty password if you sign-up with google or microsoft account.
             Please change it as soon as possible.
+          </FormHelperText>
+        ) : (
+          <FormHelperText
+            error={
+              PasswordLenError ||
+              PasswordLowError ||
+              PasswordUpperError ||
+              PasswordSpecialError
+            }
+          >
+            {userInfo.Password === "" ? (
+              "" // 不显示任何内容
+            ) : (
+              <Box>
+                {PasswordLenError ? (
+                  <Box sx={{ display: "flex", alignItems: "center" }}>
+                    <ErrorIcon fontSize="small" /> {PasswordLenMessage}
+                  </Box>
+                ) : (
+                  <Box
+                    sx={{
+                      display: "flex",
+                      alignItems: "center",
+                      color: green[500]
+                    }}
+                  >
+                    <CorrectIcon fontSize="small" /> {PasswordLenMessage}
+                  </Box>
+                )}
+                {PasswordLowError ? (
+                  <Box sx={{ display: "flex", alignItems: "center" }}>
+                    <ErrorIcon fontSize="small" /> {PasswordLowMessage}
+                  </Box>
+                ) : (
+                  <Box
+                    sx={{
+                      display: "flex",
+                      alignItems: "center",
+                      color: green[500]
+                    }}
+                  >
+                    <CorrectIcon fontSize="small" /> {PasswordLowMessage}
+                  </Box>
+                )}
+                {PasswordUpperError ? (
+                  <Box sx={{ display: "flex", alignItems: "center" }}>
+                    <ErrorIcon fontSize="small" /> {PasswordUpperMessage}
+                  </Box>
+                ) : (
+                  <Box
+                    sx={{
+                      display: "flex",
+                      alignItems: "center",
+                      color: green[500]
+                    }}
+                  >
+                    <CorrectIcon fontSize="small" /> {PasswordUpperMessage}
+                  </Box>
+                )}
+                {PasswordSpecialError ? (
+                  <Box sx={{ display: "flex", alignItems: "center" }}>
+                    <ErrorIcon fontSize="small" /> {PasswordSpecialMessage}
+                  </Box>
+                ) : (
+                  <Box
+                    sx={{
+                      display: "flex",
+                      alignItems: "center",
+                      color: green[500]
+                    }}
+                  >
+                    <CorrectIcon fontSize="small" /> {PasswordSpecialMessage}
+                  </Box>
+                )}
+              </Box>
+            )}
           </FormHelperText>
         )}
       </FormControl>
@@ -362,7 +705,7 @@ function ProfileForm({ userInfo, setUserInfo }) {
           onResolve={({ provider, data }) => {
             console.log(provider);
             console.log(data);
-            handleGoogleLinkChange(data.email);
+            updateGoogleLink(data.sub);
           }}
           onReject={(err) => {
             console.log(err);
@@ -371,7 +714,7 @@ function ProfileForm({ userInfo, setUserInfo }) {
           <GoogleLoginButton>Link with Google account</GoogleLoginButton>
         </LoginSocialGoogle>
       ) : (
-        <Typography>Linked with: {userInfo.Linked_Account.Google}</Typography>
+        <Typography>Linked with: Google ID - {userInfo.Linked_Account.Google}</Typography>
       )}
 
       {/* Microsoft Link */}
@@ -383,7 +726,7 @@ function ProfileForm({ userInfo, setUserInfo }) {
           onResolve={({ provider, data }) => {
             console.log(provider);
             console.log(data);
-            handleMicrosoftLinkChange(data.mail);
+            updateMicrosoftLink(data.id);
           }}
           onReject={(err) => {
             console.log(err);
@@ -395,11 +738,72 @@ function ProfileForm({ userInfo, setUserInfo }) {
         </LoginSocialMicrosoft>
       ) : (
         <Typography>
-          Linked with: {userInfo.Linked_Account.Microsoft}
+          Linked with: Microsoft ID - {userInfo.Linked_Account.Microsoft}
         </Typography>
       )}
     </Stack>
   );
+}
+
+// Validate Email
+function validateEmail(email) {
+  if (email === "") {
+    return true;
+  }
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+// Validate Password
+function validatePasswordLen(password) {
+  // 长度为8-16个字符
+  const lengthRegex = /^.{8,16}$/;
+  if (!lengthRegex.test(password)) {
+    return false;
+  }
+
+  return true;
+}
+
+function validatePasswordLow(password) {
+  // 至少有一个小写字符
+  const lowercaseRegex = /[a-z]/;
+  if (!lowercaseRegex.test(password)) {
+    return false;
+  }
+
+  return true;
+}
+
+function validatePasswordUpper(password) {
+  // 至少有一个大写字符
+  const uppercaseRegex = /[A-Z]/;
+  if (!uppercaseRegex.test(password)) {
+    return false;
+  }
+
+  return true;
+}
+
+function validatePasswordSpecial(password) {
+  // 至少有一个特殊字符，这里使用自定义的特殊字符列表
+  const specialChars = "!@#$%^&*()_+{}[\\]:\";'<>?,./|\\\\`~=-";
+  const specialRegex = new RegExp(`[${specialChars}]`);
+  if (!specialRegex.test(password)) {
+    return false;
+  }
+
+  return true;
+}
+
+// Validate Confirm Password
+function validateConfirmPassword(password, confirmPassword) {
+  if (password === confirmPassword) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 ProfileAvatar.propTypes = {
